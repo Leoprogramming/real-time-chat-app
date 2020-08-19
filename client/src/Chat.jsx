0125a99a71ad8775bf262ff1c6bb8401091a5ac6
Leonardo Costa <leo.costa.programming@gmail.com>
2020-08-19 15:19:50 +0200

    imported useQuery, gql, created const GET_MESSAGES, const Chat

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,8 +1,37 @@
 import React from "react";
-
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  uri: "https://localhost:4000/",
   cache: new InMemoryCache(),
 });
+
+const GET_MESSAGES = gql`
+  query {
+    messages {
+      id
+      content
+      user
+    }
+  }
+`;
+
+const Chat = () => {
+  return (
+    <div>
+      <Messages user="Jack" />
+    </div>
+  );
+};
+
+export default () => (
+  <ApolloProvider client={client}>
+    <Chat />
+  </ApolloProvider>
+);
